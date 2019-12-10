3d8ed53e39b8a451faaffcceca1b2ea70940b82d
Admin <565172499@qq.com>
2019-12-10 19:11:53 +0800

    1

--- app/src/main/java/gp/ms/com/ui/MainActivity.java
+++ app/src/main/java/gp/ms/com/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package gp.ms.com.maosen;
+package gp.ms.com.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gp.ms.com.maosen.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
