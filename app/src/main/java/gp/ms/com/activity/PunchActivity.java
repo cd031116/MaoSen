c9b1624d4f1a26862938c390f4b6e1da95743474
lyj <565172499@qq.com>
2019-12-11 17:31:06 +0800

    1

---
+++ app/src/main/java/gp/ms/com/activity/PunchActivity.java
@@ -0,0 +1,29 @@
+package gp.ms.com.activity;
+
+import android.os.Bundle;
+
+import gp.ms.com.R;
+import gp.ms.com.base.BaseActivity;
+
+public class PunchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_punch;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
